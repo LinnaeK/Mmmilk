9d44c9275fc33a4cd4c9b5dd0f8d614d5b8860ec
linnaek <linnaekraemer@gmail.com>
2020-01-19 23:21:58 -0600

    add navbar with selection capabilities. Needs age selection added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,50 @@
 import React, { Component } from 'react';
 import Nav from './components/NavBar/NavBar'
 import './App.css';
-import NavBar from './components/NavBar/NavBar';
+// import NavBar from './components/NavBar/NavBar';
+// import ResponsiveDrawer from './components/NavBar/ResponsiveDrawer';
 import LoginPage from './components/LoginPage/LoginPage'
 import { Route, Switch, Redirect } from 'react-router-dom'
 import SignupPage from './pages/SignupPage/SignupPage';
 import userService from './utils/userService';
+import ResponsiveDrawer from './components/ResponsiveDrawer/ResponsiveDrawer';
 
 class App extends Component {
   constructor(){
     super()
     this.state = {
-      user: userService.getUser()
+      user: userService.getUser(),
+      comparisonByItem: 'Country',
+      country:[]
     }
   }
 
+  handleRadioClick = (e) => {
+    e.persist()
+    this.setState({country:[]})
+    this.setState({[e.target.name]:e.target.value})
+  }
+
+  handleCountryClick = (e) => {
+    e.persist()
+    if(this.state.comparisonByItem==='Country'){
+      console.log('ran country')
+      this.setState({country:[e.target.value]})
+    }else{
+      console.log('ran this')
+      let currentCountrySelection = [...this.state.country]
+      if(this.state.country.some((country)=>{return e.target.value===country})){
+        let filteredCountries = this.state.country.filter((country)=>{return country!=e.target.value})
+        this.setState({country: filteredCountries})
+      }else{
+        currentCountrySelection.push(e.target.value)
+        console.log(currentCountrySelection)
+        this.setState({country: currentCountrySelection})
+      }
+    }
+  }
+
+  
   handleSignupOrLogin = () => {
     this.setState({user: userService.getUser()});
   }
@@ -29,7 +59,12 @@ class App extends Component {
       <div>
         <Switch>
           <Route exact path='/' render={() =>
-          <NavBar user={this.state.user} handleLogout={this.handleLogout}/>  
+          <ResponsiveDrawer 
+            user={this.state.user} 
+            handleLogout={this.handleLogout} 
+            handleRadioClick={this.handleRadioClick}
+            handleCountryClick={this.handleCountryClick}
+            />  
         }/>
         <Route exact path='/signup' render={({ history }) =>
           <SignupPage
---
+++ src/components/SelectCountry/SelectCountry.jsx
@@ -0,0 +1,113 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Select from '@material-ui/core/Select';
+
+
+const useStyles = makeStyles(theme => ({
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+        maxWidth: 300,
+    },
+    chips: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    chip: {
+        margin: 2,
+    },
+    noLabel: {
+        marginTop: theme.spacing(3),
+    },
+}));
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+    PaperProps: {
+        style: {
+            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+            width: 250,
+        },
+    },
+};
+
+const names = [
+    'United States',
+    'Canada',
+    'England',
+    'Chile',
+    'Egypt',
+    'Brazil',
+    'Japan',
+    'Congo',
+    'Mexico',
+    'Romania'
+]
+
+function getStyles(name, personName, theme) {
+    return {
+        fontWeight:
+        personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+    };
+}
+
+export default function SelectCountry(props) {
+    const classes = useStyles();
+    const theme = useTheme();
+    const [personName, setPersonName] = React.useState([]);
+  
+    const handleChange = event => {
+      setPersonName(event.target.value);
+    };
+  
+    const handleChangeMultiple = event => {
+      const { options } = event.target;
+      const value = [];
+      for (let i = 0, l = options.length; i < l; i += 1) {
+        if (options[i].selected) {
+          value.push(options[i].value);
+        }
+      }
+      setPersonName(value);
+    };
+  
+
+SelectCountry.propTypes = {
+    
+};
+    return (
+        <div>
+           <FormControl className={classes.formControl} >
+            <InputLabel shrink htmlFor="select-multiple-native" name="country">
+            Countries
+            </InputLabel>
+            <Select
+                multiple
+                native
+                value={personName}
+                name="country"
+                onChange={handleChangeMultiple}
+                // onClick={props.handleRadioClick}
+                inputProps={{
+                    id: 'select-multiple-native',
+                }}
+                >
+                {names.map(named => (
+                    <option key={named} value={named} name="country" onClick={props.handleCountryClick} >
+                        {named}
+                        </option>
+                ))}
+            </Select>
+        </FormControl> 
+        </div>
+    );
+}
+  
